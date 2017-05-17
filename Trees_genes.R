bd51491c59b0aca2acae40cc5b43007b3afbe5ee
dtsh2 <davidtshayman@gmail.com>
2017-05-17 19:10:49 +1200

    edits for plotting

--- Trees_genes.R
+++ Trees_genes.R
@@ -1,7 +1,33 @@
+rm(list = ls())
 library(phytools)
-setwd('C:/Users/David Hayman/Downloads/Re__paper_idea')
-
-tree = read.newick("hominis_hsp70_MLtree.newick")
-
+library(ape) 
 # https://joey711.github.io/phyloseq/plot_tree-examples.html
 
+pdf("gp60.pdf",width = 7,height = 7)
+par(mfrow=c(1,2))
+tree = read.newick("SPSgp60.newick")
+x<-tree$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[3]))
+tree$tip.label<-x1
+#tree<-ladderize(tree)
+colorCodes <- c(rainbow(length(unique(tree$tip.label))))
+names(colorCodes)<-unique(tree$tip.label)
+plotTree(tree,ftype="off",main='Species')
+add.scale.bar(x=0.5,y=1)
+tiplabels(#x1, # if text
+  col = colorCodes[tree$tip.label],bg=NA,frame = 'none',pch=16)
+text(x=500,'Species')
+
+tree = read.newick("SPSgp60.newick")
+x<-tree$tip.label
+x1 = sapply(strsplit(x, split='_', fixed=TRUE),function(x) (x[2]))
+tree$tip.label<-x1
+#tree<-ladderize(tree)
+colorCodes <- c(rainbow(length(unique(tree$tip.label))))
+names(colorCodes)<-unique(tree$tip.label)
+plotTree(tree,ftype="off")
+add.scale.bar(x=0.5,y=1)
+tiplabels(#x1, # if text
+  col = colorCodes[tree$tip.label],bg=NA,frame = 'none',pch=16)
+text(x=500,'Host')
+dev.off()
